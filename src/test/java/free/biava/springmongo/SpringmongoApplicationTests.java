c7cc58d8d1bea6e9752c01d9132c8ea19572ec78
Gustavo Biava <gustavo.rbiava@hotmail.com>
2024-01-26 01:20:07 -0300

    Project created

---
+++ src/test/java/free/biava/springmongo/SpringmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package free.biava.springmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
