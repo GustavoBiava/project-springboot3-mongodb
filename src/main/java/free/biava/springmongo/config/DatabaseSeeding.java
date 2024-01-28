84991795d541bddd54bd1d7a466fe1788e463672
Gustavo Biava <gustavo.rbiava@hotmail.com>
2024-01-28 15:13:13 -0300

    Post entity

--- src/main/java/free/biava/springmongo/config/DatabaseSeeding.java
+++ src/main/java/free/biava/springmongo/config/DatabaseSeeding.java
@@ -1,30 +1,43 @@
 package free.biava.springmongo.config;
 
+import java.time.Instant;
+import java.time.temporal.TemporalAmount;
+import java.time.temporal.TemporalUnit;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import free.biava.springmongo.entities.Post;
 import free.biava.springmongo.entities.User;
+import free.biava.springmongo.repositories.PostRepository;
 import free.biava.springmongo.repositories.UserRepository;
 
 @Configuration
 public class DatabaseSeeding implements CommandLineRunner{
-
+	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private PostRepository postRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
 		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User user = new User(null, "Ronaldo", "cristiano@gmail.com");
 		User user2 = new User(null, "Messi", "lional@gmail.com");
 		User user3 = new User(null, "Mbappé", "kylian@gmail.com");
 		
+		Post post = new Post(null, Instant.now(), "Partiu Miami!", "Hoje é o dia da viagem para Miami para os amistosos de pré-temporada! Siuu", user);
+		Post post2 = new Post(null, Instant.now(), "Esqueci minhas chuteiras!", "Lembrei-me agora que minhas chuteiras ficaram no CT do clube! Terei que dar um jeito", user);
+		
 		userRepository.saveAll(Arrays.asList(user, user2, user3));
+		postRepository.saveAll(Arrays.asList(post, post2));
 		
 	}
 
