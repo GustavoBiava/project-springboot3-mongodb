90a3eadf0aeba9e1edff87528095235da1270f41
Gustavo Biava <gustavo.rbiava@hotmail.com>
2024-01-27 22:37:19 -0300

    User delete & DatabaseException

--- src/main/java/free/biava/springmongo/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/free/biava/springmongo/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import free.biava.springmongo.services.exceptions.DatabaseException;
 import free.biava.springmongo.services.exceptions.ResourceNotFoundExcepetion;
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -17,6 +18,13 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundExcepetion RnFoundException, HttpServletRequest request) {
 		HttpStatus status = HttpStatus.NOT_FOUND;
 		StandardError standardError = new StandardError(Instant.now(), status.value(), RnFoundException.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(standardError);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> databaseError(DatabaseException dbException, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError standardError = new StandardError(Instant.now(), status.value(), dbException.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(standardError);
 	}
 
--- src/main/java/free/biava/springmongo/services/UserService.java
+++ src/main/java/free/biava/springmongo/services/UserService.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoDataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import free.biava.springmongo.dto.UserDTO;
 import free.biava.springmongo.entities.User;
 import free.biava.springmongo.repositories.UserRepository;
+import free.biava.springmongo.services.exceptions.DatabaseException;
 import free.biava.springmongo.services.exceptions.ResourceNotFoundExcepetion;
 
 @Service
@@ -28,6 +30,20 @@ public class UserService {
 	
 	public User insert(User user) {
 		return repository.insert(user);
+	}
+	
+	public void delete(String id) {
+		if(repository.existsById(id)) {
+			try {
+				repository.deleteById(id);		
+			}
+			catch(MongoDataIntegrityViolationException error) {
+				throw new DatabaseException(error.getMessage());
+			}
+		}
+		else {
+			throw new ResourceNotFoundExcepetion(id);
+		}
 	}
 	
 	public User fromDTO(UserDTO userDTO) {
