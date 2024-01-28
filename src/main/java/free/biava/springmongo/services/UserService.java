2904df0ddb27be5b6464c30350419ca0967604c3
Gustavo Biava <gustavo.rbiava@hotmail.com>
2024-01-27 22:08:16 -0300

    User insert

--- src/main/java/free/biava/springmongo/services/UserService.java
+++ src/main/java/free/biava/springmongo/services/UserService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import free.biava.springmongo.dto.UserDTO;
 import free.biava.springmongo.entities.User;
 import free.biava.springmongo.repositories.UserRepository;
 import free.biava.springmongo.services.exceptions.ResourceNotFoundExcepetion;
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = repository.findById(id);
 		return user.orElseThrow(() -> new ResourceNotFoundExcepetion(id));
+	}
+	
+	public User insert(User user) {
+		return repository.insert(user);
+	}
+	
+	public User fromDTO(UserDTO userDTO) {
+		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
 	}
 
 }
